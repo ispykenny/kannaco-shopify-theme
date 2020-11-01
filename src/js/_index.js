fc643e5e112c96bacdf58a43ad3bc5dec7c2efdf
ispykenny <kenny.krosky@gmail.com>
2020-11-01 15:48:26 -0800

    Feat: Added slider for Testimonial

--- src/js/_index.js
+++ src/js/_index.js
@@ -1,22 +1,22 @@
-import lazyLoad from './lazy-load/index';
-import _dropDown from './_drop-down';
-import useCaseSlide from './use-case-slide';
-import {runAboutSlider, accordion} from './about';
-import initCategorySlider from './category-slider';
-import ajaxToCart from './ajax-to-cart';
-import _craftLogo from './_craft-logo';
-import prodSliderInit from './single_product-slider';
-import productAccordion from './accordion.js';
-import byob from './_byob';
-
+import lazyLoad from "./lazy-load/index";
+import _dropDown from "./_drop-down";
+import useCaseSlide from "./use-case-slide";
+import { runAboutSlider, accordion } from "./about";
+import initCategorySlider from "./category-slider";
+import ajaxToCart from "./ajax-to-cart";
+import _craftLogo from "./_craft-logo";
+import prodSliderInit from "./single_product-slider";
+import productAccordion from "./accordion.js";
+import byob from "./_byob";
+import createTestimonialSlide from "./testimonial-slide";
 
 byob();
 ajaxToCart();
 
 $(window).on({
-  'load': () => {
-    $('body').addClass('loaded');
-    if($('.two-col-offset').length >= 2) {
+  load: () => {
+    $("body").addClass("loaded");
+    if ($(".two-col-offset").length >= 2) {
       useCaseSlide();
     }
     runAboutSlider();
@@ -24,38 +24,38 @@ $(window).on({
     // setTimeout(() => initCategorySlider() , 200)
     _craftLogo();
 
-    if($('.product-single')) {
+    if ($(".product-single")) {
       prodSliderInit();
       productAccordion();
     }
-  }
-})
 
-import {showPasswordReset, justShowPassword} from './password-reset';
+    createTestimonialSlide();
+  },
+});
+
+import { showPasswordReset, justShowPassword } from "./password-reset";
 showPasswordReset();
 
+$("#init-password-reset").on("click", justShowPassword);
 
-$('#init-password-reset').on('click', justShowPassword);
-
-
-import mobileMenu from './_mobile-menu';
+import mobileMenu from "./_mobile-menu";
 mobileMenu();
 
-import threeColSlider from './_three-col';
+import threeColSlider from "./_three-col";
 threeColSlider();
 
-import footer from './footer';
-import qty from './qty';
+import footer from "./footer";
+import qty from "./qty";
 footer();
 
-if($('.qty').length > 0) {
-  qty()
+if ($(".qty").length > 0) {
+  qty();
 }
 
-import scroll from './scroll';
-import search from './search';
-import batch from './batch';
-import setFeaturedBlogMarkup from './featured-blogs';
+import scroll from "./scroll";
+import search from "./search";
+import batch from "./batch";
+import setFeaturedBlogMarkup from "./featured-blogs";
 
 scroll();
 
@@ -63,15 +63,13 @@ search();
 batch();
 setFeaturedBlogMarkup();
 
-
-
-document.querySelectorAll('.address-delete-form').forEach(deleteForm => {
-  deleteForm.addEventListener('submit', event => {
-    const confirmMessage = event.target.getAttribute('data-confirm-message');
+document.querySelectorAll(".address-delete-form").forEach((deleteForm) => {
+  deleteForm.addEventListener("submit", (event) => {
+    const confirmMessage = event.target.getAttribute("data-confirm-message");
 
     if (
       !window.confirm(
-        confirmMessage || 'Are you sure you wish to delete this address?'
+        confirmMessage || "Are you sure you wish to delete this address?"
       )
     ) {
       event.preventDefault();
@@ -79,4 +77,4 @@ document.querySelectorAll('.address-delete-form').forEach(deleteForm => {
   });
 });
 
-$('.category__list li').on('click', event => accordion(event));
+$(".category__list li").on("click", (event) => accordion(event));
--- src/js/ajax-to-cart.js
+++ src/js/ajax-to-cart.js
@@ -1,41 +1,39 @@
-export default function() {
-  let $btn = $('#AddToCart');
+export default function () {
+  let $btn = $("#AddToCart");
   let $btnCurrentText = $btn.text();
 
-
-  const addToCart = event => {
-    let bubble = $('#cart-element__qty span');
-    let total;    
+  const addToCart = (event) => {
+    let bubble = $("#cart-element__qty span");
+    let total;
     event.preventDefault();
-    
-    if(bubble.text().length > 0) {
+
+    if (bubble.text().length > 0) {
       let bubbleValue = parseInt(bubble.text());
-       total = bubbleValue + parseInt($("#Quantity").val());
+      total = bubbleValue + parseInt($("#Quantity").val());
     } else {
       total = parseInt($("#Quantity").val());
     }
-    
-    if(total <= 10) {
+
+    if (total <= 10) {
       $("#cart-element__qty").fadeIn();
       bubble.text(total);
-      
-      $btn.text('Added!')
-      $.post('/cart/add.js', 
-          {
-            quantity: $("#Quantity").val(),
-            id: $('[name="id"]').val()
-          },
-          null,
-          'json'
-        
+
+      $btn.text("Added!");
+      $.post(
+        "/cart/add.js",
+        {
+          quantity: $("#Quantity").val(),
+          id: $('[name="id"]').val(),
+        },
+        null,
+        "json"
       );
 
-      setTimeout(() => $btn.text($btnCurrentText), 1200)
+      setTimeout(() => $btn.text($btnCurrentText), 1200);
     } else {
-      alert('Unable to add to cart. Exceeded limit (10) items')
+      alert("Unable to add to cart. Exceeded limit (10) items");
     }
+  };
 
-  }
-  
-  $("#AddToCartForm").on('submit', event => addToCart(event))
+  // $("#AddToCartForm").on('submit', event => addToCart(event))
 }
