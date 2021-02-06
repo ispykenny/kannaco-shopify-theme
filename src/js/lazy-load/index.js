b2bfeeb750c63a642deb13c7095712d5250c02e7
Kenny Krosky <kenny.krosky@gmail.com>
2021-02-05 21:33:17 -0800

    code cleanup

--- src/js/lazy-load/index.js
+++ src/js/lazy-load/index.js
@@ -1,13 +1,27 @@
 import { setBackgroundImages } from "./backgrounds";
 import { getImageRatio , initDomImages } from "./images";
 const all_images = document.querySelectorAll('img');
+const article = document.querySelector('.article');
 
-getImageRatio(all_images);
+if(article) {
+  console.log('didnt init lazy load')
+}else {
+  getImageRatio(all_images);
+}
 if(all_images.length) {
   
   window.onload = () => {
     let allImages = document.querySelectorAll('img');
-    initDomImages(allImages)
+    if(article) {
+      allImages.forEach((img) =>  {
+        img.style.opacity = 1
+        img.style.width = 'auto'
+        img.style.maxWidth = '100%'
+      })
+    } else {
+      initDomImages(allImages)
+    }
+
     setBackgroundImages();
   };
 }
